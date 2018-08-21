852b3c1ac8efb0e98af9c25203daf2e4c153e386
sksat <sksat.tec@gmail.com>
2018-08-21 23:14:28 +0900

    update parse test

--- parse.c
+++ parse.c
@@ -1,25 +1,35 @@
 #include <stddef.h>
 #include "parse.h"
 
+void parse_impl(ast_t* parent, vector_t* token_list){
+	size_t i;
+	ast_t* sub;
+	token_t* tok;
+
+	for(i=0; i<token_list->size; i++){
+		tok = vector_get(token_list, i);
+		switch(tok->type){
+			case tType:
+				sub = ast_new();
+				sub->type = aDefVar;
+				vector_push_back(sub->node, tok); // 型
+				i++;
+				tok = vector_get(token_list, i);
+				vector_push_back(sub->node, tok); // 変数名
+				i++;
+				vector_push_back(parent->node, sub);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
 ast_t* parse(vector_t *token_list){
 	ast_t *global = ast_new();
-	token_t *tok;
-
 	global->type = aGlobal;
-	tok = vector_get(token_list, 0);
-	switch(tok->type){
-		case tType:
-			{
-				ast_t* defvar = ast_new();
-				defvar->type  = aDefVar;
-				vector_push_back(defvar->node, tok); // 型
-				vector_push_back(defvar->node, vector_get(token_list, 1)); // 変数名
-				vector_push_back(global->node, defvar);
-			}
-			break;
-		default:
-			break;
-	}
+
+	parse_impl(global, token_list);
 
 	return global;
 }
