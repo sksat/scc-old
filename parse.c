5fd4b0c907fcc682e8e133d0b1a2b32c21fd0ed6
sksat <sksat.tec@gmail.com>
2018-08-23 17:23:25 +0900

    split func

--- parse.c
+++ parse.c
@@ -1,9 +1,33 @@
 #include <stddef.h>
 #include "parse.h"
 
+size_t parse_expr(ast_t* parent, vector_t* token_list, size_t i){
+	ast_t* expr = ast_new();
+	expr->type = aExpr;
+	token_t* tok;
+	size_t read = 0;
+	size_t k;
+	for(k=i;i<token_list->size;k++){
+		tok = vector_get(token_list, k);
+		read++;
+		if(tok->type == tSemicolon) break;
+	}
+	// token_listの[i]〜[k]が式のtoken
+
+	// とりあえずexpr->nodeに詰めておく
+	for(size_t tmp=i;tmp!=k;tmp++){
+		tok = vector_get(token_list, tmp);
+		vector_push_back(expr->node, tok);
+	}
+
+	vector_push_back(parent->node, expr);
+	return read;
+}
+
 size_t parse_block(ast_t* parent, vector_t* token_list, size_t i){
 	ast_t *ast, *expr;
 	token_t* tok = vector_get(token_list, i);
+	size_t read = 0;
 	switch(tok->type){
 		case tBlockStart:
 			error("not implemented: child block.\n");
@@ -16,23 +40,16 @@ size_t parse_block(ast_t* parent, vector_t* token_list, size_t i){
 			// node[2]: 式のAST
 			ast = ast_new();
 			ast->type = aDefVar;
-			vector_push_back(ast->node, tok);		// 型
-			tok = vector_get(token_list, i+1);	// 変数名
+			vector_push_back(ast->node, tok);			// 型
+			tok = vector_get(token_list, i+1); read++;	// 変数名
 			vector_push_back(ast->node, tok);
-			tok = vector_get(token_list, i+2); // ; or =
+			tok = vector_get(token_list, i+2); read++;	// ; or =
 			if(tok->type != tSemicolon){
 				if(!string_cmp(tok->str, to_string("="))) error("expected \'=\'\n");
-				expr = ast_new();
-				expr->type = aExpr;
-				for(size_t k=i+2;k<token_list->size;k++){
-					tok = vector_get(token_list, k);
-					if(tok->type == tSemicolon) break;
-					vector_push_back(expr->node, tok);
-				}
-				vector_push_back(ast->node, expr);
+				read += parse_expr(ast, token_list, i+3);
 			}
 			vector_push_back(parent->node, ast);
-			return 2;
+			return read;
 		default:
 			printf("unknown token: ");
 			string_print(tok->str);
