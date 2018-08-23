f6e2b99811a2de65ad40887f0a58a4a3c71751db
sksat <sksat.tec@gmail.com>
2018-08-23 16:55:39 +0900

    add parse def-var expr

--- parse.c
+++ parse.c
@@ -2,7 +2,7 @@
 #include "parse.h"
 
 size_t parse_block(ast_t* parent, vector_t* token_list, size_t i){
-	ast_t* ast;
+	ast_t *ast, *expr;
 	token_t* tok = vector_get(token_list, i);
 	switch(tok->type){
 		case tBlockStart:
@@ -11,17 +11,26 @@ size_t parse_block(ast_t* parent, vector_t* token_list, size_t i){
 		case tBlockEnd:
 			return 0;
 		case tType: // 変数宣言
+			// node[0]: 型
+			// node[1]: 変数名
+			// node[2]: 式のAST
 			ast = ast_new();
 			ast->type = aDefVar;
 			vector_push_back(ast->node, tok);		// 型
 			tok = vector_get(token_list, i+1);	// 変数名
 			vector_push_back(ast->node, tok);
-			tok = vector_get(token_list, i+2);	// ; or =
-			if(tok->type == tSemicolon){}
-			else if(tok->type == tOperator){
-
+			tok = vector_get(token_list, i+2); // ; or =
+			if(tok->type != tSemicolon){
+				if(!string_cmp(tok->str, to_string("="))) error("expected \'=\'\n");
+				expr = ast_new();
+				expr->type = aExpr;
+				for(size_t k=i+2;k<token_list->size;k++){
+					tok = vector_get(token_list, k);
+					if(tok->type == tSemicolon) break;
+					vector_push_back(expr->node, tok);
+				}
+				vector_push_back(ast->node, expr);
 			}
-
 			vector_push_back(parent->node, ast);
 			return 2;
 		default:
@@ -35,8 +44,6 @@ size_t parse_block(ast_t* parent, vector_t* token_list, size_t i){
 ast_t* parse(vector_t *token_list){
 	ast_t *global = ast_new();
 	global->type = aGlobal;
-
-//	parse_impl(global, token_list);
 
 	size_t i;
 	for(i=0; i<token_list->size; i++){
